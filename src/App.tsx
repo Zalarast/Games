6afc7ac48841d48c721f0e08f39045c5c290ddc5
Zalarast <shubin.bodya@gmail.com>
2022-06-10 15:42:30 +0300

    Сделал первичные роуты, создал страницы: Главную и Не найдено, главную - немного кастомизировал

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Main, NotFound } from "./pages";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Main.tsx
@@ -0,0 +1,7 @@
+import "../CSS/Main.css"
+export default function Main () {
+    return (<div className="Main">
+        <h1 className="title">Welcome</h1>
+        <button>Сапёр</button>
+    </div>)
+}
