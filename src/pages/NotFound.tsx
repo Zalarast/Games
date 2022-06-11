b22662a79682d043144b8fc313b6778049242e38
Zalarast <shubin.bodya@gmail.com>
2022-06-11 22:03:33 +0300

    Сделал страницу для неверного роута, слегка стилизовал, добавил кнопку возврата в главное меню

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+import "../CSS/NotFound.css";
+
 export default function NotFound() {
   return (
-    <div>
-      <h1>NotFound!</h1>
+    <div className="NotFound">
+      <h1 className="title">Упс, ничего не найдено</h1>
+      <Link to="/" className="backToMenu">Вернуться на главную</Link>
     </div>
   );
 }
